f4e73f7f238416bf75dacb83d068e6d20042357a
CassianoSF <cassianosfranco@gmail.com>
2018-04-22 20:53:21 -0300

    WIP

--- src/App.js
+++ src/App.js
@@ -1,35 +1,56 @@
 import React, { Component } from 'react';
 
 import {Jumbotron, Table, Container, Row, Col, Form, FormGroup, Label, Input, Button} from 'reactstrap'
+import  { ListGroup, ListGroupItem }  from 'reactstrap'
 
 class App extends Component {
   constructor(props){
     super(props);
 
+    this.handleChange = this.handleChange.bind(this)
+    this.generate = this.generate.bind(this)
+    this.onDrag = this.onDrag.bind(this)
+    this.onDrop = this.onDrop.bind(this)
+
+
     this.state = {
-      cache: [...Array(8).keys()].map( (i, index) =>  { return (
-        {
-            endereco: ("000" + index.toString(2)).slice(-3),
-            palavras: [...Array(4).keys()].map(i => Math.random().toString(16).substring(7) )
-        }
-      )}
-      ),
-      principal:[...Array(256).keys()].map( (i, index) =>  { return (
-        {
-            endereco: ("00000000" + index.toString(2)).slice(-8),
-            palavras: [...Array(1).keys()].map(i => Math.random().toString(16).substring(7) )
-        }
-      )}
-      ),
+      cache: this.generate(16),
+      principal: this.generate(256),
       estatisticas: {},
       endereco_leitura: '',
       endereco_escrita: '',
-      dado: ''
+      Data: ''
     } 
 
-    this.handleChange = this.handleChange.bind(this)
   }
 
+  generate(n_blocks){
+    return [...Array(n_blocks).keys()].map( (i, index) =>  { return (
+      {
+          tag: ("00000000" + index.toString(2)).slice(-8),
+          words: [...Array(1).keys()].map(i => Math.random().toString(16).substring(7).toUpperCase() )
+      }
+    )})
+  }
+
+  read(){
+
+  }
+
+  write(){
+
+  }
+
+  statistics(){
+
+  }
+
+  onDrag(event){
+  }
+
+  onDrop(event){
+    console.log(event.target)
+  }
 
   handleChange(event) {
     this.setState({[event.target.name]: event.target.value});
@@ -40,16 +61,21 @@ class App extends Component {
     event.preventDefault();
   }
 
-
-  desenho_palavra(palavra){
-    return(<th>{palavra}</th>)
+  word(word, block){
+    return(<th block={block.tag}>{word}</th>)
   }
 
-  desenho_bloco(bloco){
+  block(block, type){
+    let tag_n = parseInt(block.tag, 2)
+    let r = tag_n
+    let g = 50 - tag_n
+    let b = 33
+    let style = { backgroundColor: "rgba(" + r +","+ g +","+ b + ", 1)"
+    } 
     return (
-        <tr>
-            <th>{bloco.endereco}</th>
-            {bloco.palavras.map(palavra => this.desenho_palavra(palavra))}
+        <tr draggable="true" style={style} onDrag={this.onDrag} onDrop={this.onDrop}>
+            <th block={block.tag}>{block.tag}</th>
+            {block.words.map(word => this.word(word, block))}
         </tr>
     )
   }
@@ -66,7 +92,7 @@ class App extends Component {
 		                        <Input 
 		                            type="text" 
 		                            name="endereco_leitura" 
-		                            placeholder="Endereço" 
+		                            placeholder="Tag" 
 		                            value={this.state.endereco_leitura} 
 		                            onChange={this.handleChange} 
 		                        />
@@ -79,15 +105,15 @@ class App extends Component {
 		                        <Input 
 		                            type="text" 
 		                            name="endereco_escrita" 
-		                            placeholder="Endereço" 
+		                            placeholder="Tag" 
 		                            value={this.state.endereco_escrita} 
 		                            onChange={this.handleChange}
 		                        />
 		                        <Input 
 		                            type="text" 
-		                            name="dado" 
-		                            placeholder="Dado" 
-		                            value={this.state.dado} 
+		                            name="Data" 
+		                            placeholder="Data" 
+		                            value={this.state.Data} 
 		                            onChange={this.handleChange}
 		                        />
 		                    </FormGroup>
@@ -98,55 +124,51 @@ class App extends Component {
                 </Form>
             </Jumbotron>
             <Row>
-                <Col md="8">
+                <Col md={{size:"4", offset: 0}}>
                 	<Jumbotron>
 	                    <h4>Memoria Cache</h4>
 	                    <Table>
 	                        <thead>
 	                            <tr>
-	                                <th>Endereço</th>
-	                                <th>D0</th>
-	                                <th>D1</th>
-	                                <th>D2</th>
-	                                <th>D3</th>
+	                                <th>Tag</th>
+	                                <th>Data</th>
 	                            </tr>
 	                        </thead>
 	                        <tbody>
-	                            {this.state.cache.map(bloco => this.desenho_bloco(bloco))}
+	                            {this.state.cache.map(block => this.block(block, "cache"))}
 	                        </tbody>
 	                    </Table>
                 	</Jumbotron>
 
-                    <Jumbotron>
-	                    <h4>Estatísticas</h4>
-	                    <Table>
-	                        <tbody>
-                                <tr><th>Numero de acessos:</th></tr>
-                                <tr><th>Numero de acertos:</th></tr>
-                                <tr><th>Numero de falhas:</th></tr>
-                                <tr><th>Numero de leituras:</th></tr>
-                                <tr><th>Numero de escritas:</th></tr>
-                                <tr><th>Numero de acertos na leitura:</th></tr>
-                                <tr><th>Numero de acertos na escrita:</th></tr>
-                                <tr><th>Numero de falhas na leituras:</th></tr>
-                                <tr><th>Numero de falhas na escrita:</th></tr>
-	                        </tbody>
-	                    </Table>
-                	</Jumbotron>
                 </Col>
 
-                <Col md="4">
+                <Col md={{size:"4", offset: 0}}>
+                  <h4>Estatísticas</h4>
+                  <ListGroup>
+                    <ListGroupItem>Numero de acessos:</ListGroupItem>
+                    <ListGroupItem>Numero de acertos:</ListGroupItem>
+                    <ListGroupItem>Numero de falhas:</ListGroupItem>
+                    <ListGroupItem>Numero de leituras:</ListGroupItem>
+                    <ListGroupItem>Numero de escritas:</ListGroupItem>
+                    <ListGroupItem>Numero de acertos na leitura:</ListGroupItem>
+                    <ListGroupItem>Numero de acertos na escrita:</ListGroupItem>
+                    <ListGroupItem>Numero de falhas na leituras:</ListGroupItem>
+                    <ListGroupItem>Numero de falhas na escrita:</ListGroupItem>
+                  </ListGroup>
+                </Col>
+
+                <Col md={{size:"4", offset: 0}}>
                 	<Jumbotron>
 	                    <h4>Memoria Principal</h4>
 	                    <Table>
 	                        <thead>
 	                            <tr>
-	                                <th>Endereço</th>
-	                                <th>Dado</th>
+	                                <th>Tag</th>
+	                                <th>Data</th>
 	                            </tr>
 	                        </thead>
 	                        <tbody>
-	                            {this.state.principal.map(bloco => this.desenho_bloco(bloco))}
+	                            {this.state.principal.map(block => this.block(block, "principal"))}
 	                        </tbody>
 	                    </Table>
                 	</Jumbotron>
