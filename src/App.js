0e94ad7500abb858aa8d6d100c352b250106c953
CassianoSF <cassianosfranco@gmail.com>
2018-05-01 18:57:06 -0300

    WIP

--- src/App.js
+++ src/App.js
@@ -7,21 +7,24 @@ class App extends Component {
   constructor(props){
     super(props);
 
-    this.handleChange = this.handleChange.bind(this)
-    this.generate     = this.generate.bind(this)
-    this.onDragEnter  = this.onDragEnter.bind(this)
-    this.handleChange = this.handleChange.bind(this)
-    this.onMouseDown  = this.onMouseDown.bind(this)
-    this.onMouseUp    = this.onMouseUp.bind(this)
+    // this.onDragEnter  = this.onDragEnter.bind(this)
+    // this.handleChange = this.handleChange.bind(this)
+    // this.onMouseDown  = this.onMouseDown.bind(this)
+    // this.onMouseUp    = this.onMouseUp.bind(this)
+    
+
+    this.handleChangeAddress = this.handleChangeAddress.bind(this)
+    this.handleChangeData    = this.handleChangeData.bind(this)
     this.write        = this.write.bind(this)
     this.read         = this.read.bind(this)
     this.simulate         = this.simulate.bind(this)
 
+
     this.state = {
-          cache: this.generate(16, true),
-           main: this.generate(256, false),
+          cache: this.generateCache(16, true),
+           main: this.generateMain(256*4),
      statistics: {},
-       register: {tag: "00000000", data: "FFFFFFFF"},
+       register: {address: "0000000000", data: "FF"},
            hits: 0,
          misses: 0,
           reads: 0,
@@ -34,27 +37,39 @@ class App extends Component {
       setTimeout( () => {
         this.setState({
           register: {
-            tag: sequential ? ("00000000" + index.toString(2)).slice(-8) : ("00000000" + parseInt(Math.random()*(2**8)).toString(2)).slice(-8),
-            data: ("00000000" + parseInt(Math.random()*(2**32)).toString(16)).slice(-8).toUpperCase()
+            address: sequential ? ("0000000000" + index.toString(2)).slice(-10) : ("0000000000" + parseInt(Math.random()*(2**10)).toString(2)).slice(-10),
+            data: ("00" + parseInt(Math.random()*(2**8)).toString(16)).slice(-8).toUpperCase()
           }
         })
         read_or_write()
-      }, index * 120)
+      }, index * 100)
     })
   }
 
-  generate(n_blocks, cache){
+  generateCache(n_blocks, cache){
     return [...Array(n_blocks).keys()].map( (i, index) =>  { return (
       {
-          tag: cache ? "" : ("00000000" + index.toString(2)).slice(-8),
-          data: cache ? "" : Math.random().toString(16).substring(7).toUpperCase()
+          tag: ("00000000" + index.toString(2)).slice(-8),
+          d0: ("00" + Math.random().toString(16).substring(13).toUpperCase()).slice(-2),
+          d1: ("00" + Math.random().toString(16).substring(13).toUpperCase()).slice(-2),
+          d2: ("00" + Math.random().toString(16).substring(13).toUpperCase()).slice(-2),
+          d3: ("00" + Math.random().toString(16).substring(13).toUpperCase()).slice(-2)
+      }
+    )})
+  }
+
+  generateMain(n_blocks){
+    return [...Array(n_blocks).keys()].map( (i, index) =>  { return (
+      {
+          address: ("0000000000" + index.toString(2)).slice(-10),
+          data: ("00" + Math.random().toString(16).substring(13).toUpperCase()).slice(-2)
       }
     )})
   }
 
   write(){
     let reg = this.state.register
-    if (!this.state.cache.filter(i => i.tag === reg.tag)[0]){
+    if (!this.state.cache.filter(i => i.tag === reg.tag.slice(0,8))[0]){
       this.randomTrade(reg)
     }else{
       this.setState({
@@ -65,10 +80,6 @@ class App extends Component {
       main: this.merge(this.state.main, reg),
       writes: this.state.writes + 1
     })
-  }
-
-  merge(memory, reg){
-    return memory.map(block => (block.tag == reg.tag) ? reg : block)
   }
 
   read(){
@@ -88,6 +99,10 @@ class App extends Component {
     }
   }
 
+  merge(memory, reg){
+    return memory.map(block => (block.tag == reg.tag.slice(0,8)) ? reg : block)
+  }
+
   randomTrade(new_block){
     let rand = parseInt(Math.random() * 16)
     this.setState({
@@ -96,48 +111,45 @@ class App extends Component {
     })
   }
 
-  onDragEnter(event, block, type){
-    this.setState({
-      type: type,
-      register: {tag: block.tag, data: this.state.register.data}
-    })
-  }
+  // onDragEnter(event, block, type){
+  //   this.setState({
+  //     type: type,
+  //     register: {tag: block.tag, data: this.state.register.data}
+  //   })
+  // }
 
-  onDragEnd(block, type){
-    if(type !== this.state.type){
-      if(type === "main"){
-        this.read()
-      }else{
-        this.write()
-      }
-    }
-  }
+  // onDragEnd(block, type){
+  //   if(type !== this.state.type){
+  //     if(type === "main"){
+  //       this.read()
+  //     }else{
+  //       this.write()
+  //     }
+  //   }
+  // }
 
-  onMouseDown(block, type){
-    this.setState({
-      register: block
-    })
-  }
+  // onMouseDown(block, type){
+  //   this.setState({
+  //     register: block
+  //   })
+  // }
 
-  onMouseUp(block, type){
-    this.read()
-  }
-
-
-  handleErrors(target){
-    return (
-      (target.name==="tag" && target.value.split("").filter(i => i != "0" && i != "1")[0])
-    )
-  }
+  // onMouseUp(block, type){
+  //   this.read()
+  // }
   
-  handleChange(event) {
-    let value = event.target.value.match(/[0-9A-Fa-f]{0,10}/g)[0].toUpperCase()
-    let hex   = ("00000000" + value).slice(-8)
-    if (!this.handleErrors(event.target)){
-      this.setState({
-        register: update(this.state.register, {[event.target.name]: {$set: hex}})
-      });
-    }
+  handleChangeAddress(event) {
+    let value = ("0000000000" + event.target.value.match(/[0-1]{0,11}/g)[0].toUpperCase()).slice(-10)
+    this.setState({
+      register: update(this.state.register, {[event.target.name]: {$set: value}})
+    });
+  }
+
+  handleChangeData(event) {
+    let value = ("00" + event.target.value.match(/[0-9A-Fa-f]{0,3}/g)[0].toUpperCase()).slice(-2)
+    this.setState({
+      register: update(this.state.register, {[event.target.name]: {$set: value}})
+    });
   }
 
   handleSubmit(event) {
@@ -145,58 +157,49 @@ class App extends Component {
     event.preventDefault();
   }
 
-  word(block){
-    return(<th block={block.tag}>{block.data && block.data.match(/.{1,2}/g).join(" | ")}</th>)
-  }
-
-  cell(data, block){
-    return(<th block={block}>{("00"+data.toUpperCase()).slice(-2)}</th>)
-  }
-
-  block(block, type){
+  frame(block){
     let tag_n = parseInt(block.tag, 2)
     let r = tag_n
     let g = 50 - tag_n
     let b = 33
     let style = { backgroundColor: "rgba(" + r +","+ g +","+ b + ", 1)" }
-    if (type == "main"){
-      return(
-        block.data.match(/.{1,2}/g).map((cell, index) =>{
-          return(
-            <tr block={block.tag} 
-                draggable="true" 
-                style={style} 
-                onDragEnter={(event) => this.onDragEnter(event, block, type)}
-                onDragEnd={() =>  this.onDragEnd(block, type)}
-                onMouseDown={() => this.onMouseDown (block)}
-                onMouseUp={() => this.onMouseUp (block)}
+    return (
+      <tr block={block.tag} 
+          draggable="true" 
+          style={style} 
+          onDragEnter={(event) => this.onDragEnter(event, block, "cache")}
+          onDragEnd={() =>  this.onDragEnd(block, "cache")}
+          onMouseDown={() => this.onMouseDown (block)}
+          onMouseUp={() => this.onMouseUp (block)}
+          >
+        <th>{block.tag}</th>
+        <th>{block.d0}</th>
+        <th>{block.d1}</th>
+        <th>{block.d2}</th>
+        <th>{block.d3}</th>
+      </tr>
+    )
+  }
 
-                >
-
-              <th>{block.tag + ("00" + index.toString(2)).slice(-2)}</th>
-              {this.cell(cell, block)}
-            </tr>
-
-          )
-        })
-      )
-    }else{
-      return (
-          <tr block={block.tag} 
-              draggable="true" 
-              style={style} 
-              onDragEnter={(event) => this.onDragEnter(event, block, type)}
-              onDragEnd={() =>  this.onDragEnd(block, type)}
-              onMouseDown={() => this.onMouseDown (block)}
-              onMouseUp={() => this.onMouseUp (block)}
-
-              >
-
-            <th>{block.tag}</th>
-            {this.word(block)}
-          </tr>
-      )
-    }
+  cell(cell){
+    let tag_n = parseInt(parseInt(cell.address, 2)/4)
+    let r = tag_n
+    let g = 50 - tag_n
+    let b = 33
+    let style = { backgroundColor: "rgba(" + r +","+ g +","+ b + ", 1)" }
+     return (
+      <tr cell={cell.address} 
+          draggable="true" 
+          style={style} 
+          onDragEnter={(event) => this.onDragEnter(event, cell, "main")}
+          onDragEnd={() =>  this.onDragEnd(cell, "main")}
+          onMouseDown={() => this.onMouseDown (cell)}
+          onMouseUp={() => this.onMouseUp (cell)}
+          >
+        <th>{cell.address}</th>
+        <th>{cell.data}</th>
+      </tr>
+    )
   }
 
   render() {
@@ -220,10 +223,10 @@ class App extends Component {
                   <Jumbotron>
                     <h4>Read/Write</h4>
                     <FormGroup>
-                      <Label className="m-4" for="examplePassword">Tag</Label>
-                      <Input onKeyPress={(e) => e.key === 'Enter' ? this.read() : null } onChange={this.handleChange} value={this.state.register.tag} type="text" name="tag" />
+                      <Label className="m-4" for="examplePassword">Address</Label>
+                      <Input onKeyPress={(e) => e.key === 'Enter' ? this.read() : null } onChange={this.handleChangeAddress} value={this.state.register.address} type="text" name="address" />
                       <Label className="m-4" for="examplePassword">Data</Label>
-                      <Input onKeyPress={(e) => e.key === 'Enter' ? this.write() : null } onChange={this.handleChange} value={this.state.register.data} type="text" name="data" />
+                      <Input onKeyPress={(e) => e.key === 'Enter' ? this.write() : null } onChange={this.handleChangeData} value={this.state.register.data} type="text" name="data" />
                     </FormGroup>
                     <Button onClick={this.read} className="m-2">Read</Button>
                     <Button onClick={this.write} className="m-2">Write</Button>
@@ -238,35 +241,37 @@ class App extends Component {
                   <Jumbotron>
                       <h4>Cache Memory</h4>
                       <Table>
-                          <thead>
-                              <tr>
-                                  <th>Tag</th>
-                                  <th>D0 | D1 | D2 | D3</th>
-                              </tr>
-                          </thead>
-                          <tbody>
-                              {this.state.cache.map(block => this.block(block, "cache"))}
-                          </tbody>
+                        <thead>
+                          <tr>
+                            <th>Tag</th>
+                            <th>D0</th>
+                            <th>D1</th>
+                            <th>D2</th>
+                            <th>D3</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                            {this.state.cache.map(frame => this.frame(frame))}
+                        </tbody>
                       </Table>
                   </Jumbotron>
-
                 </Col>
 
                 <Col md={{size:"4", offset: 0}}>
-                	<Jumbotron>
-	                    <h4>Main Memory</h4>
-	                    <Table>
-	                        <thead>
-	                            <tr>
-	                                <th>Address</th>
-	                                <th>Data</th>
-	                            </tr>
-	                        </thead>
-	                        <tbody>
-	                            {this.state.main.map(block => this.block(block, "main"))}
-	                        </tbody>
-	                    </Table>
-                	</Jumbotron>
+                  <Jumbotron>
+                      <h4>Main Memory</h4>
+                      <Table>
+                        <thead>
+                          <tr>
+                            <th>Address</th>
+                            <th>Data</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                            {this.state.main.map(cell => this.cell(cell))}
+                        </tbody>
+                      </Table>
+                  </Jumbotron>
                 </Col>
             </Row>
         </Container>
